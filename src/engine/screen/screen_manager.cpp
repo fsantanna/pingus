15e15865e4cf5251939c54cb241effc83c4f86ac
fsantanna <francisco.santanna@gmail.com>
2017-03-05 10:32:51 -0300

    (>) & aliases for calls/fins

--- src/engine/screen/screen_manager.cpp
+++ src/engine/screen/screen_manager.cpp
@@ -32,27 +32,6 @@ using tinygettext::Log;
 
 extern void SDL_Driver_update (float delta);
 
-FramebufferSurface* load_framebuffer_sdl_surface(const Pathname& filename, ResourceModifier::Enum modifier)
-{
-  // FIXME: Implement proper cache
-  try
-  {
-    Surface surface(filename);
-    if (modifier != ResourceModifier::ROT0)
-    {
-      surface = surface.mod(modifier);
-    }
-    return new FramebufferSurface(new SDLFramebufferSurfaceImpl(surface.get_surface()));
-  }
-  catch(const std::exception& err)
-  {
-    // return a dummy surface for cases where the image file can't be found
-    //log_error("%1%", err.what());
-    Surface surface(Pathname("images/core/misc/404.png", Pathname::DATA_PATH));
-    return new FramebufferSurface(new SDLFramebufferSurfaceImpl(surface.get_surface()));
-  }
-}
-
 ScreenManager* ScreenManager::instance_ = 0;
 
 ScreenManager::ScreenManager()
